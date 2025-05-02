2d42e23ff84ea8653588d16dae89a7ea51f8b342
陈华 <2394439184@qq.com>
2025-05-02 16:55:25 +0800

    基类角色创建，顺带着添加忽略的文件

---
+++ Source/Aura_Game/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,27 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/Aura_Game/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,27 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+/**
+ * 这是一个游戏角色的基类，用于定义游戏角色的基础属性和方法。
+ */
+UCLASS()
+class AURA_GAME_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

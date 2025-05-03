886ebbf3135d84849cf092d37f88970de7194df2
陈华 <2394439184@qq.com>
2025-05-03 11:31:58 +0800

    创建GAS的基础属性集和Component

---
+++ Source/Aura_Game/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,28 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ *	艾拉的AbilitySystemComponent
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AURA_GAME_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAuraAbilitySystemComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
---
+++ Source/Aura_Game/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,23 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+// 标准属性宏
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+/**
+ * Aura属性集
+ */
+UCLASS()
+class AURA_GAME_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+};

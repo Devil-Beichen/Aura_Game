a6243be18b26f5515fb70f3f2d993b4036ec4435
陈华 <2394439184@qq.com>
2025-05-03 11:25:15 +0800

    添加玩家的状态

---
+++ Source/Aura_Game/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f; // 设置网络更新频率为100Hz
+}
---
+++ Source/Aura_Game/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 玩家状态
+ */
+UCLASS()
+class AURA_GAME_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};

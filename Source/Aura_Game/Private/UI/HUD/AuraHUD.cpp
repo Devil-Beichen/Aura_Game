22e3495dfa83654f3b85669f9096d491ea824a0b
陈华 <2394439184@qq.com>
2025-05-03 21:49:18 +0800

    HUD添加

---
+++ Source/Aura_Game/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,15 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
---
+++ Source/Aura_Game/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,30 @@
+﻿// 这是一款名为《元素之主：艾拉》的游戏项目，所有者是逸辰
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * Aura的HUD类，用于处理游戏界面的显示和交互。
+ */
+UCLASS()
+class AURA_GAME_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	// 显示小组件
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// 小组件类
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+};
